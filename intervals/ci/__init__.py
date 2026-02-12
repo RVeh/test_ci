251ca341c73609adfa89dfd09fa785795e6cd64b
veh <110566592+RVeh@users.noreply.github.com>
2026-02-12 06:42:53 +0100

    Update __init__.py

--- intervals/ci/__init__.py
+++ intervals/ci/__init__.py
@@ -1,16 +1,1 @@
-"""
-CI – Confidence Intervals
-
-Geometric representation, calculations, and simulations.
-"""
-
-from .ellipse import plot_ci_ellipse
-from .plot import plot_ci
-from .simulation import simulate_ci
-
-__all__ = [
-    "plot_ci_ellipse",
-    "plot_ci",
-    "simulate_ci",
-]
 
